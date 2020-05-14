d2e0eda37d3a1f9fd382b7bae933697827626d40
jonoton <65074561+jonoton@users.noreply.github.com>
2020-05-14 11:22:11 -0400

    render. dir studio.
    * Modified dir studio to have less hard coded. Added
      user input for options.
    * Updated for formatting.

--- render.bat
+++ render.bat
@@ -1,14 +1,12 @@
 @echo off
 
 set SRC_FOLDER=%~f1
-echo Source Folder is %SRC_FOLDER%
+echo Source Folder: %SRC_FOLDER%
 
 set DST_FOLDER=%~f2
-echo Destination Folder is %DST_FOLDER%
+echo Destination Folder: %DST_FOLDER%
 
 powershell.exe -Command "%~dp0utils\DirStudio.ps1" %SRC_FOLDER% %DST_FOLDER%
-
-dir %DST_FOLDER%
 
 echo.
 echo Done!
--- utils/DirStudio.ps1
+++ utils/DirStudio.ps1
@@ -1,59 +1,119 @@
+
+$fusionFilename = "FusionStudio_x64.exe"
+
+function Get-ProgramFiles-FullPath {
+	param($filename)
+	$filePath = dir -Path "C:\\Program Files\\" -Filter $filename -Recurse -ErrorAction SilentlyContinue -Force | % { $_.FullName }
+	return $filePath
+}
+
+$dirStudioExe = Get-ProgramFiles-FullPath -filename $fusionFilename
+write-host "Fusion Studio: $dirStudioExe"
+
+function Human-Readable-Size {
+	param ([int]$size)
+	if ($size -gt 1TB) { return [string]::Format("{0:0.00} TB", $size / 1TB) }
+	elseif ($size -gt 1GB) { return [string]::Format("{0:0.00} GB", $size / 1GB) }
+	elseif ($size -gt 1MB) { return [string]::Format("{0:0.00} MB", $size / 1MB) }
+	elseif ($size -gt 1KB) { return [string]::Format("{0:0.00} kB", $size / 1KB) }
+	elseif ($size -gt 0) { return [string]::Format("{0:0.00} B", $size) }
+	else { return "" }
+}
+
+function Pretty-Print-Directory {
+	param($path)
+	$files = dir $path
+	if ($files -and $files.length -ne 0) {
+		write-host $path
+		$totalLength = 0
+		foreach ($file in $files) {
+			$totalLength = $totalLength + $file.length
+			$filename = $file.name
+			$hsize = Human-Readable-Size -size $file.length
+			write-host "`t$filename`t$hsize"
+		}
+		$htotal = Human-Readable-Size -size $totalLength
+		write-host "Total $htotal"
+	}
+}
+
+function Print-Separator {
+	$separator = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	write-host $separator
+}
+
 $processDate = (Get-Date).ToString("yyyy-MM-dd")
-write-host "Process Date $processDate"
+write-host "Process Date: $processDate"
 
 $frontDir = "Front"
 $backDir = "Back"
 $frontFilePre = "GPFR"
 $backFilePre = "GPBK"
 
-$3k = "3072"
-$5k = "5760"
+$useGpu = "--iq 3"
+$useCpu = "--iq 2"
+$hw = $useGpu
+$askHw = read-host "Choose HW:`n`t1. GPU`n`t2. CPU`n?(1)"
+if ($askHw -eq 2) {
+	$hw = $useCpu
+}
 
-$dirStudioExe = "C:\Program Files\GoPro\Fusion Studio 1.3\FusionStudio_x64.exe"
-$width = $3k
+$dWarp = "--pc 1"
+$multiBandBlending = "--blending 0"
+$equirectangularProjection = "--projection 0"
+$fullStabilization = "--stabilization 2"
+$styling = "--flatMode 0 --globalContrast 128 --sharpness 128 --temperature 128 --tint 128"
+
+$3k = "--width 3072 --videoCodec 0" # h.264
+$4k = "--width 3840 --videoCodec 0" # h.264
+$5k = "--width 5120 --videoCodec 1" # cineform
+$resolution = $3k
+$askRes = read-host "Choose resolution:`n`t1. 3k h.264`n`t2. 4k h.264`n`t3. 5k cineform`n?(1)"
+if ($askRes -eq 2) {
+	$resolution = $4k
+}
+elseif ($askRes -eq 3) {
+	$resolution = $5k
+}
+
+$globalSettings = "$resolution $dWarp $multiBandBlending $equirectangularProjection $fullStabilization $hw $styling"
 
 $srcDir = $args[0]
 $destDir = $args[1].ToString() + "\$processDate"
-write-host "Destination Folder is $destDir"
-if (!(test-path $destDir))
-{
-	mkdir $destDir
-	write-host ""
+write-host "Destination Folder: $destDir"
+if (!(test-path $destDir)) {
+	mkdir $destDir > $null
 }
 
 $subName = ".tmp"
-
 $destDirSub = "$destDir\$subName"
-write-host "Destination Sub Folder is $destDirSub"
-
-if (!(test-path $destDirSub))
-{
-	mkdir $destDirSub
-	write-host ""
+write-host "Destination Sub Folder: $destDirSub"
+if (!(test-path $destDirSub)) {
+	mkdir $destDirSub > $null
 }
-write-host "$destDir contains"
-dir $destDir
-write-host ""
+Pretty-Print-Directory -path $destDir
 
 $files = dir $srcDir\$frontDir\*.mp4
 $stopWatch = new-object system.diagnostics.stopwatch
 $stopWatch.Start()
 $countVideos = 0
-foreach ($file in $files)
-{
+foreach ($file in $files) {
 	$filename = $file.ToString()
 	$len = $filename.length
-	$videoNum = $filename.Substring($len-8, 4)
-	write-host "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	$videoNum = $filename.Substring($len - 8, 4)
+	Print-Separator
 	write-host "Processing Video $videoNum"
 	cp $srcDir\$frontDir\$frontFilePre$videoNum.* $destDirSub
 	cp $srcDir\$backDir\$backFilePre$videoNum.* $destDirSub
-	dir $destDirSub
-	."$dirStudioExe" -d $destDirSub --wave $destDirSub\$backFilePre$videoNum.wav --amb 1 --width $width --videoCodec 0 --pc 1 --blending 0 --projection 0 --stabilization 2 --iq 3 --flatMode 0 --globalContrast 128 --sharpness 128 --temperature 128 --tint 128 -o $destDirSub  | Out-Null
+	Pretty-Print-Directory -path $destDirSub
+	$studioParamsStr = "-d `"$destDirSub`" --wave `"$destDirSub\$backFilePre$videoNum.wav`" --amb 1 $globalSettings -o `"$destDirSub`""
+	$studioParams = $studioParamsStr.split(" ")
+	write-host $dirStudioExe $studioParams
+	."$dirStudioExe" $studioParams | Out-Null # Wait with Out-Null
 	rm $destDirSub\*
 	move "$destDir\_shot000$subName" "$destDir\Video$videoNum.mp4"
 	write-host "Resulting Output is $destDir\Video$videoNum.mp4"
-	write-host "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	Print-Separator
 	write-host ""
 	$countVideos++
 }
@@ -63,26 +123,21 @@ $elapsedMinutes = $stopWatch.Elapsed.TotalMinutes
 $elapsedHours = $stopWatch.Elapsed.TotalHours
 
 write-host ""
-write-host "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-if ($elapsedSeconds -lt 60)
-{
+Print-Separator
+if ($elapsedSeconds -lt 60) {
 	write-host "Total of $countVideos Videos took $elapsedSeconds seconds"
 }
-elseif ($elapsedMinutes -lt 60)
-{
+elseif ($elapsedMinutes -lt 60) {
 	write-host "Total of $countVideos Videos took $elapsedMinutes minutes"
 }
-else
-{
+else {
 	write-host "Total of $countVideos Videos took $elapsedHours hours"
 }
-write-host "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+Print-Separator
 write-host ""
 
-if (test-path $destDirSub)
-{
+if (test-path $destDirSub) {
 	rm $destDirSub
 }
 
-dir $destDir
-write-host ""
+Pretty-Print-Directory -path $destDir
